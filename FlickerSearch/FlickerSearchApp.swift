7741665268ea92d2cc8f44f5bdd7eab2a28de82a
Ranjith vanaparthi <rvanaparthi@firstrepublic.com>
2024-06-27 22:27:05 -0700

    Initial Commit

---
+++ FlickerSearch/FlickerSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickerSearchApp.swift
+//  FlickerSearch
+//
+//  Created by Ranjith vanaparthi on 6/27/24.
+//
+
+import SwiftUI
+
+@main
+struct FlickerSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
